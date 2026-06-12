cd29bba793c5f64395e69173aa1d632beb6b3779
Souhayb120 <souhaybhadi06@gmail.com>
2026-06-12 12:12:54 +0100

    jkhj

--- src/main/java/com/example/HealthCare/Config/RedisCacheConfig.java
+++ src/main/java/com/example/HealthCare/Config/RedisCacheConfig.java
@@ -3,7 +3,9 @@ package com.example.HealthCare.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
@@ -14,6 +16,9 @@ public class RedisCacheConfig {
     public RedisCacheConfiguration cacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues();
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
 }
+
